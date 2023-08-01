fd8c77fb484dbbf35d671a3e3f7d2d5bc7f2f497
Matthew Wynn <matthew@matthewwynn.com>
2023-07-31 23:00:31 -0700

    Add slightly better UX around adding and removing

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,10 @@
-import '../styles/tailwind.css';
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import "../styles/tailwind.css";
+config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }) {
-    return (
-        <Component {...pageProps} />
-    );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
