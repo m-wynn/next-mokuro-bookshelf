fae7945f79637f64349d473792acd69a744aed15
Adam <56907039+adamjsturge@users.noreply.github.com>
2023-12-30 15:29:48 -0800

    Make AdminLayour Async

--- app/(application)/admin/layout.tsx
+++ app/(application)/admin/layout.tsx
@@ -1,6 +1,6 @@
 import Sidebar from "./sidebar";
 import { getSession } from "lib/session";
-export default function AdminLayout({
+export default async function AdminLayout({
   children, // will be a page or nested layout
 }: {
   children: React.ReactNode;
