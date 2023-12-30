c02e985e5f180c550a3bd51a7009f7f7c66e6f0f
Adam <56907039+adamjsturge@users.noreply.github.com>
2023-12-30 15:27:10 -0800

    Fix Idor for Admin page

--- app/(application)/admin/layout.tsx
+++ app/(application)/admin/layout.tsx
@@ -1,9 +1,14 @@
 import Sidebar from "./sidebar";
+import { getSession } from "lib/session";
 export default function AdminLayout({
   children, // will be a page or nested layout
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getSession("GET");
+  if (session.user.role !== "ADMIN") {
+    throw new Error("Unauthorized");
+  }
   return (
     <section className="drawer drawer-open">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
