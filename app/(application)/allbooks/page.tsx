12792d4ea043b36ad4298b51da8721e032551e15
Matthew Wynn <matthew@matthewwynn.com>
2024-01-11 22:02:03 -0800

    UX improvements mostly for volume adding

--- app/(application)/allbooks/page.tsx
+++ app/(application)/allbooks/page.tsx
@@ -1,6 +1,7 @@
 import prisma from 'db';
 import { Prisma } from '@prisma/client';
 import { getSession } from 'lib/session';
+import Link from 'next/link';
 import { AllBooks } from './AllBooks';
 
 const seriesSelect = (userId: string) => ({
@@ -25,6 +26,9 @@ const seriesSelect = (userId: string) => ({
         select: { pages: true },
       },
     },
+    orderBy: {
+      number: 'asc',
+    },
   },
 }) satisfies Prisma.SeriesSelect;
 
@@ -40,7 +44,22 @@ const AddNew = async () => {
 
   return (
     <div className="p-5 latte bg-crust text-text">
-      <AllBooks series={series} />
+      {series.length === 0 ? (
+        <div className="flex flex-col justify-center items-center">
+          <h1 className="text-4xl">No Series Found</h1>
+          {session.user?.role === 'ADMIN' ? (
+            <p className="text-2xl">
+              Start by <Link href="/admin/volumes" className="underline">adding a series</Link>
+            </p>
+          ) : (
+            <p className="text-2xl">
+              Your admin needs to add a series before you can add a book.
+            </p>
+          )}
+        </div>
+      ) : (
+        <AllBooks series={series} />
+      )}
     </div>
   );
 };
